454467ddc9c532d4d7410d32410557e2088581bf
kzhao <kin.zhao@mlb.com>
2016-12-23 09:52:53 -0500

    cleanup

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,22 +1,2 @@
 
 // Global Client Side JS Code Here...
-
-// $("li").click(function(e) {
-//     e.preventDefault();
-//     $("ul .active").removeClass("active");
-//     $(this).addClass("active");
-// });
-
-// $(document).ready(function(){
-//     $("li").click(function() {
-//       $("ul .active").removeClass("active");
-//       $(this).addClass("active");
-//    });
-// });
-$(document).ready(function () {
-    var url = window.location;
-    $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
-    $('ul.nav a').filter(function() {
-      return this.href == url;
-    }).parent().addClass('active');
-});
--- src/index.jsx
+++ src/index.jsx
@@ -3,54 +3,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
 
-const MainComponent = React.createClass({
+const user = {
+  firstName: 'John',
+  lastName: 'Smith'
+};
 
-    getInitialState: function() {
-        return {
-            cartItems: []
-        };
-    },
-
-    loadDataFromServer: function () {
-        var that = this;
-
-        window.getSnacks().then(function(snackRecords) {
-            var snack = {}; 
-            var snacks = [];
-            
-            for (var key in snackRecords) {
-                snack = snackRecords[key];
-                snack.id = key;
-                snacks.push(snack);
-            }
-
-            console.log(snacks);
-            
-            that.setState({ cartItems: snacks });
-        });
-    },
-
-    componentDidMount: function() {
-        this.loadDataFromServer();
-    },
-
-    onSuccess: function() {
-        this.loadDataFromServer();
-    },
-
-    render: function() {
-        console.log('rendering component...');
-
-        return (
-            <div className='components-container'>
-            </div>
-        );
-    }
-});
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
 
 ReactDOM.render(
-    <MainComponent/>,
+    element,
     document.getElementById('main-component')
 );
 
